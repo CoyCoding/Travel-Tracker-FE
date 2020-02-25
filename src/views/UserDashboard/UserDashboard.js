44d90faf894ed0a479615a3df72f1fa9a41921fa
Matt Coy <fluffytoycoy@gmail.com>
2020-02-25 11:02:40 -0600

    added mapbox

--- src/views/UserDashboard/UserDashboard.js
+++ src/views/UserDashboard/UserDashboard.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react';
+import Map from './Map/Map';
 import { connect } from 'react-redux';
 import { getUserInfo } from '../../actions/userActions';
 import './UserDashboard.scss';
 
 const UserDashboard = (props) => {
-  useEffect(()=>{
-    props.getUserInfo();
-    console.log(props.user)
-  },[]);
+  const useEffectOnce = (fn) => useEffect(fn, []);
+
+  useEffectOnce(()=>{
+    if(!props.user){
+      props.getUserInfo();
+    }
+  });
+
   console.log(props)
   return (
     <div className="user-dash">
       <div className="sidebar">
       </div>
       <div className="map platformed">
+        <Map/>
       </div>
     </div>
   )
