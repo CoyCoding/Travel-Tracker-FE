fa997eb8c95ddb386a46c5c915cc636fb786327e
Matt Coy <fluffytoycoy@gmail.com>
2020-02-26 13:50:42 -0600

    messing with map marker and popup

--- src/views/UserDashboard/Map/Map.js
+++ src/views/UserDashboard/Map/Map.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
-
-const TOKEN = 'pk.eyJ1IjoiZmx1ZmZ5dG95Y295IiwiYSI6ImNrNzIfQ.WsxTXH54_UnRveYVwN6gNQ';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import LocationIcon from './data/LocationIcon';
+const TOKEN = 'pk.eyNQ';
 
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
+    width: '100%',
+    height: '100%',
+    latitude: 38.7577,
+    longitude: -90.4376,
     zoom: 8
   });
-
+  const [showPopup, togglePopup] = useState(true);
   return (
-    <ReactMapGL mapboxApiAccessToken={TOKEN}
-      {...viewport}
-      onViewportChange={setViewport}
-    />
+    <ReactMapGL mapboxApiAccessToken={TOKEN} {...viewport} onViewportChange={setViewport}>
+      <Marker latitude={38.78} longitude={-90.41} offsetLeft={-8} offsetTop={-8} >
+        <LocationIcon onClick={()=> togglePopup(true)}/>
+      </Marker>
+      {showPopup && <Popup
+      latitude={38.78}
+      longitude={-90.41}
+      offsetTop={-2}
+      offsetLeft={8}
+      closeButton={true}
+      closeOnClick={false}
+      captureScroll={true}
+      onClose={() => togglePopup(!showPopup)}
+      anchor="left" >
+      <div>You are here</div>
+      <p>More stuff</p>
+      <p>More stuff</p>
+      <p>More stuff</p>
+      </Popup>}
+    </ReactMapGL>
   );
 }
 
